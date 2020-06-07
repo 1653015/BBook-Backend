31cecf43775421656982a9ccbc0e19bcfb0cfe36
sonpham11235 <50816481+sonpham11235@users.noreply.github.com>
2020-06-07 17:36:43 +0700

    update recovery link

--- router/user/auth.js
+++ router/user/auth.js
@@ -140,7 +140,7 @@ router.post('/forgot', (req, res, next) => {
                 subject: 'Node.js Password Reset',
                 text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
                     'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
-                    'http://' + req.headers.host + '/auth/reset/' + token + '\n\n' +
+                    'https://1653015.github.io/BBook-Frontend/#/reset/' + token + '\n\n' +
                     'If you did not request this, please ignore this email and your password will remain unchanged.\n'
             };
             smtpTransport.sendMail(mailOptions, function(err) {
